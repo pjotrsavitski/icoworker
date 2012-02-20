a4f6da9eaf7efc2be3fcaf0816a720582aa79b85
Pjotr Savitski <pjotr.savitski@gmail.com>
2012-02-20 11:55:38 +0000

    Language choice is preserved upon logout

--- actions/logout.php
+++ actions/logout.php
@@ -3,8 +3,14 @@
         
     if (isset($_SESSION["user"])) {
         unset($_SESSION["user"]);
+        if (isset($_SESSION['language'])) {
+            $language = $_SESSION['language'];
+        }
         session_destroy();
         session_start();
+        if (isset($language)) {
+            $_SESSION['language'] = $language;
+        }
 	} else {
 		$TeKe->add_system_message(_("No authenticated user found."), 'error');
 	}
